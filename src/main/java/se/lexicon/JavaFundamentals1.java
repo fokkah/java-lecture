eba7824b131d846615d7257defb4825cc997864a
fokkah <agameesport@gmail.com>
2025-01-07 09:54:15 +0100

    Java fundamentals lecture 1

---
+++ src/main/java/se/lexicon/JavaFundamentals1.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class JavaFundamentals1 {
+
+    //Java Comments
+    // single line comment
+    /* multiline comment
+    /** This is a java doc, to create documentation java code where i can export the comments to the .doc
+     */
+    //psvm + TAB = public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // ## Variable Declaration ##
+
+    }
+}
